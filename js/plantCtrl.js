00bba617aef38d267100d9006d7f757939fb0301
Peter Kincaid <loaatao@gmail.com>
2017-09-10 17:53:09 -0500

    Added filter functionality

--- js/plantCtrl.js
+++ js/plantCtrl.js
@@ -3,8 +3,26 @@ angular
   .controller("plantCtrl", function($scope, $firebaseObject, $firebaseArray) {
     var ref = firebase.database().ref();
     var obj = $firebaseObject(ref);
-
-    $scope.data = obj.$loaded().then(function() {
-      $scope.data = obj;
+    obj.$loaded().then(function() {
+      // $scope.data = obj["Washington"]["Abies"];
+      // console.log(obj);
     });
+
+    $scope.submitInput = function(state, county) {
+      $scope.data = obj[state];
+      // console.log($scope.data);
+      // if($scope.data[state].length)
+      // console.log($scope.data.Nevada[0].county.split(","));
+      $scope.data2 = [];
+      if (county) {
+        for (var i = 0; i < $scope.data.length; i++) {
+          for (j = 0; j < $scope.data[i].county.split(",").length; j++) {
+            if ($scope.data[i].county.split(",")[j] === county) {
+              $scope.data2.push($scope.data[i]);
+            }
+          }
+        }
+        $scope.data = $scope.data2;
+      }
+    };
   });
