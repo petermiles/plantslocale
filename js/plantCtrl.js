078435d1b495701943eed3bb09e31e7d722ce446
Peter Kincaid <loaatao@gmail.com>
2017-09-11 17:55:54 -0500

    moved search and filter feature to service from controller

--- js/plantCtrl.js
+++ js/plantCtrl.js
@@ -1,21 +1,6 @@
 angular
   .module("plantsApp")
-  .controller("plantCtrl", function($scope, $firebaseObject, $firebaseArray) {
-    var ref = firebase.database().ref();
-    var obj = $firebaseObject(ref);
-
-    $scope.submitInput = function(state, county) {
-      $scope.data = obj[state];
-      $scope.data2 = [];
-      if (county) {
-        for (var i = 0; i < $scope.data.length; i++) {
-          for (j = 0; j < $scope.data[i].county.split(",").length; j++) {
-            if ($scope.data[i].county.split(",")[j] === county) {
-              $scope.data2.push($scope.data[i]);
-            }
-          }
-        }
-        $scope.data = $scope.data2;
-      }
-    };
+  .controller("plantCtrl", function($scope, plantServ) {
+    $scope.data = plantServ.data;
+    $scope.submitInput = plantServ.sortData;
   });
--- js/plantServ.js
+++ js/plantServ.js
@@ -1,9 +1,20 @@
-angular.module("plantsApp").service("plantServ"),
-  function($http) {
-    var baseUrl =
-      "https://plantslocale.firebaseio.com/.json?auth=Wx8KObP2HGrXWvQ69meaoSsDWIshlRFcTTNyvOAA";
-
-    this.getPlants = function() {
-      return $http.get(baseUrl);
+angular
+  .module("plantsApp")
+  .service("plantServ", function($firebaseObject, $firebaseArray) {
+    var ref = firebase.database().ref();
+    var obj = $firebaseObject(ref);
+    this.sortData = function(state, county) {
+      this.data = obj[state];
+      this.data2 = [];
+      if (county) {
+        for (var i = 0; i < this.data.length; i++) {
+          for (var j = 0; j < this.data[i].county.split(",").length; j++) {
+            if (this.data[i].county.split(",")[j] === county) {
+              this.data2.push(this.data[i]);
+            }
+          }
+        }
+        this.data = this.data2;
+      }
     };
-  };
+  });
