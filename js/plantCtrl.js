c8fa2b73d52d4b921b59b892616f917b6469f4e9
Peter Kincaid <loaatao@gmail.com>
2017-09-11 12:13:32 -0500

    fix small syntax

--- js/plantCtrl.js
+++ js/plantCtrl.js
@@ -3,16 +3,9 @@ angular
   .controller("plantCtrl", function($scope, $firebaseObject, $firebaseArray) {
     var ref = firebase.database().ref();
     var obj = $firebaseObject(ref);
-    obj.$loaded().then(function() {
-      // $scope.data = obj["Washington"]["Abies"];
-      // console.log(obj);
-    });
 
     $scope.submitInput = function(state, county) {
       $scope.data = obj[state];
-      // console.log($scope.data);
-      // if($scope.data[state].length)
-      // console.log($scope.data.Nevada[0].county.split(","));
       $scope.data2 = [];
       if (county) {
         for (var i = 0; i < $scope.data.length; i++) {
